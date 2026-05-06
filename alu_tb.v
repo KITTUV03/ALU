84f80886dc44898c6804d20ad6387e2da4a7999e
Patel Kittu <kittupatel@feserver.mirafra.com>
2026-05-06 10:11:56 +0530

    ALU_PROJECT

---
+++ alu_tb.v
@@ -0,0 +1,89 @@
+DTH 8
+`define VALID_M 2
+`define OPERATION 4
+
+module tb;
+    reg CLK;
+    reg RST;
+    reg [`VALID_M-1:0] INP_VALID;
+	reg  MODE; 
+    reg [`OPERATION-1:0] CMD; 
+	reg CE;
+    reg [`WIDTH-1:0] OPA;
+    reg [`WIDTH-1:0] OPB;
+    reg CIN;
+    wire ERR; 
+    wire [2*`WIDTH-1:0] RES; 
+	wire OFLOW;
+	wire COUT;
+	wire G; 
+	wire L;
+	wire E;
+  
+  ALU_DESIGN dut(CLK,RST,INP_VALID,MODE,CMD,CE,OPA,OPB,CIN,ERR,RES,OFLOW,COUT,G,L,E);
+  
+  
+  initial begin
+    CLK=1'b0;
+    forever #5 CLK=~CLK;
+  end
+  
+  task arithmetic_inputs;
+    MODE = 1'b1;
+    CE=1'b1;
+    RST = 1'b0;
+    INP_VALID = 2'b11;
+    
+    for(int i=0;i<14;i++) begin
+     //       OPA = $urandom();
+      OPA = 10;
+//       OPB = $urandom();
+            OPB = 5;
+      CMD = i;
+      CIN = $urandom();
+      @(posedge CLK);
+    end
+    
+  endtask
+  
+   task logic_inputs;
+    MODE = 1'b0;
+    CE=1'b1;
+    RST = 1'b0;
+    INP_VALID = 2'b11;
+    
+    for(int i=0;i<14;i++) begin
+//       OPA = $urandom();
+      OPA = 10;
+//       OPB = $urandom();
+      OPB = 5;
+      CMD = i;
+      CIN = $urandom();
+      @(posedge CLK);
+    end
+    
+  endtask
+  
+  task reset;
+    RST=1'b1;
+    repeat(2)
+      @(posedge CLK);
+    
+    RST=1'b0;
+  endtask
+  
+  initial begin
+    reset;
+    arithmetic_inputs;
+    logic_inputs;
+    repeat(3)
+      @(posedge CLK);
+    $finish;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  
+endmodule
