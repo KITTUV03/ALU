22c0811f27de9637c78e44a2605c3f92224dde7c
Patel Kittu <157161459+KITTUV03@users.noreply.github.com>
2026-05-11 15:22:04 +0530

    Update alu_design.v

--- src/design/alu_design.v
+++ src/design/alu_design.v
@@ -2,45 +2,45 @@
 
  
 
-/***************************************************************
-* 
-*   K     K   IIIIIII  TTTTTTT  TTTTTTT  U     U
-*   K   K       I         T        T     U     U
-*   K K         I         T        T     U     U
-*   KK          I         T        T     U     U
-*   K K         I         T        T     U     U
-*   K   K       I         T        T     U     U
-*   K     K   IIIIIII     T        T      UUUUU
-*
-***************************************************************/
+// /***************************************************************
+// * 
+// *   K     K   IIIIIII  TTTTTTT  TTTTTTT  U     U
+// *   K   K       I         T        T     U     U
+// *   K K         I         T        T     U     U
+// *   KK          I         T        T     U     U
+// *   K K         I         T        T     U     U
+// *   K   K       I         T        T     U     U
+// *   K     K   IIIIIII     T        T      UUUUU
+// *
+// ***************************************************************/
 
-/***************************************************************
-*  Author        : Kittu Patel
-*  Company       : Mirafra Software Technologies Pvt. Ltd.
-*  Project Name  : Parameterized ALU Design
-*
-*  Description   :
-*  This project implements a parameterized Arithmetic Logic Unit
-*  (ALU) supporting both arithmetic and logical operations.
-*  Features include:
-*    - Arithmetic ops: ADD, SUB, ADD with CIN, SUB with CIN
-*    - Increment/Decrement operations
-*    - Comparison (G, L, E flags)
-*    - Signed operations with overflow detection
-*    - Logical ops: AND, OR, XOR, NOT, shifts, rotate
-*    - Pipelined multiplication operations:
-*         • MUL_INC  : (OPA+1)*(OPB+1)
-*         • MUL_SHL  : (OPA<<1)*OPB
-*    - Valid signal based input control
-*    - Clock enable (CE) based operation
-*
-*  Start Date    : [04-05-2026]
-*  End Date      : [05-05-2026]
-*
-*  Notes         :
-*  - Supports parameterized data width
-*  - Suitable for ASIC/FPGA design and UVM verification
-***************************************************************/
+// /***************************************************************
+// *  Author        : Kittu Patel
+// *  Company       : Mirafra Software Technologies Pvt. Ltd.
+// *  Project Name  : Parameterized ALU Design
+// *
+// *  Description   :
+// *  This project implements a parameterized Arithmetic Logic Unit
+// *  (ALU) supporting both arithmetic and logical operations.
+// *  Features include:
+// *    - Arithmetic ops: ADD, SUB, ADD with CIN, SUB with CIN
+// *    - Increment/Decrement operations
+// *    - Comparison (G, L, E flags)
+// *    - Signed operations with overflow detection
+// *    - Logical ops: AND, OR, XOR, NOT, shifts, rotate
+// *    - Pipelined multiplication operations:
+// *         \u2022 MUL_INC  : (OPA+1)*(OPB+1)
+// *         \u2022 MUL_SHL  : (OPA<<1)*OPB
+// *    - Valid signal based input control
+// *    - Clock enable (CE) based operation
+// *
+// *  Start Date    : [04-05-2026]
+// *  End Date      : [05-05-2026]
+// *
+// *  Notes         :
+// *  - Supports parameterized data width
+// *  - Suitable for ASIC/FPGA design and UVM verification
+// ***************************************************************/
 
 
 `define VALID_M    2
@@ -109,6 +109,7 @@ module ALU_DESIGN #(parameter WIDTH = 8)
     reg                   r_CIN;
 
     localparam WID = $clog2(WIDTH);   
+    reg        [WIDTH:0]     rot_amt;
 
     reg signed [2*WIDTH-1:0] signed_result;
     reg [2*WIDTH-1:0] mulinc_s1, mulshl_s1;
@@ -158,7 +159,6 @@ module ALU_DESIGN #(parameter WIDTH = 8)
 
                 if (r_MODE) begin
                     case (r_CMD)
-
                         `ADD: begin
                             {COUT, RES[WIDTH-1:0]} <= (r_INP_VALID==`V_BOTH) ? (r_OPA + r_OPB) : {COUT, RES[WIDTH-1:0]};
                             RES   <= (r_INP_VALID==`V_BOTH) ? (r_OPA + r_OPB) : RES;
@@ -211,15 +211,21 @@ module ALU_DESIGN #(parameter WIDTH = 8)
 
                         `MUL_INC: begin
                             if (r_INP_VALID==`V_BOTH) begin
+                            ERR <= 0;
+
                                 mulinc_s1 <= (r_OPA+1)*(r_OPB+1);
                                 mulinc_v1 <= 1;
+                              RES   <= {(2*WIDTH){1'b0}};
                             end else ERR <= 1;
                         end
 
                         `MUL_SHL: begin
                             if (r_INP_VALID==`V_BOTH) begin
+                                ERR <= 0;
                                 mulshl_s1 <= (r_OPA<<1)*r_OPB;
                                 mulshl_v1 <= 1;
+                              RES   <= {(2*WIDTH){1'b0}};
+
                             end else ERR <= 1;
                         end
 
@@ -255,7 +261,9 @@ module ALU_DESIGN #(parameter WIDTH = 8)
 
                         default: begin
                             RES<=0; COUT<=0; OFLOW<=0; {G,L,E}<=`NONE;
-                        end
+                            ERR <=1;
+
+                        end 
 
                     endcase
                 end
@@ -275,43 +283,62 @@ module ALU_DESIGN #(parameter WIDTH = 8)
                         `SHR1_B: begin RES[WIDTH-1:0]<=(r_INP_VALID==`V_BOTH||r_INP_VALID==`V_B)?r_OPB>>1:0; RES[2*WIDTH-1:WIDTH]<=0; OFLOW<=0;COUT<=0;{G,L,E}<=`NONE;ERR<=~(r_INP_VALID==`V_BOTH||r_INP_VALID==`V_B);end
                         `SHL1_B: begin RES[WIDTH-1:0]<=(r_INP_VALID==`V_BOTH||r_INP_VALID==`V_B)?r_OPB<<1:0; RES[2*WIDTH-1:WIDTH]<=0; OFLOW<=0;COUT<=0;{G,L,E}<=`NONE;ERR<=~(r_INP_VALID==`V_BOTH||r_INP_VALID==`V_B);end
 
-                        `ROL_A_B: begin
-                            if (r_INP_VALID == `V_BOTH) begin
-                                if (|r_OPB[WIDTH-1:WID]) begin
-                                    ERR <= 1;
-                                end else begin
-                                    RES[WIDTH-1:0]       <= (r_OPA << r_OPB[WID-1:0]) | (r_OPA >> (WIDTH - r_OPB[WID-1:0]));
-                                    RES[2*WIDTH-1:WIDTH] <= 0;
-                                    ERR <= 0;
-                                end
-                            end else begin
-                                RES <= 0; ERR <= 1;
+                       
+                          `ROL_A_B: begin
+                            rot_amt = r_OPB[WID-1:0];
+
+                            if (|r_OPB[WIDTH-1:WID+1]) begin
+                                ERR <= 1;
+                                RES <= 0;
                             end
-                            OFLOW <= 0; COUT <= 0; {G,L,E} <= `NONE;
+
+                            else
+                                ERR <= 0;
+
+                            RES[WIDTH-1:0] <=
+                                (r_OPA << rot_amt) |
+                                (r_OPA >> (WIDTH - rot_amt));
+
+                            RES[2*WIDTH-1:WIDTH] <= 0;
+
+                            OFLOW <= 0;
+                            COUT  <= 0;
+                                {G,L,E} <= `NONE;
                         end
 
-                        `ROR_A_B: begin
-                            if (r_INP_VALID == `V_BOTH) begin
-                                if (|r_OPB[WIDTH-1:WID]) begin
+
+                           `ROR_A_B: begin
+
+                                rot_amt = r_OPB[WID-1:0];
+
+                             if (|r_OPB[WIDTH-1:WID+1]) begin
                                     ERR <= 1;
-                                end else begin
-                                    RES[WIDTH-1:0]       <= (r_OPA >> r_OPB[WID-1:0]) | (r_OPA << (WIDTH - r_OPB[WID-1:0]));
-                                    RES[2*WIDTH-1:WIDTH] <= 0;
-                                    ERR <= 0;
-                                end
-                            end else begin
-                                RES <= 0; ERR <= 1;
+                                    RES <= 0;
+                             end
+
+                                else
+
+                                RES[WIDTH-1:0] <=
+                                    (r_OPA >> rot_amt) |
+                                    (r_OPA << (WIDTH - rot_amt));
+
+                                RES[2*WIDTH-1:WIDTH] <= 0;
+
+                                OFLOW <= 0;
+                                COUT  <= 0;
+                                {G,L,E} <= `NONE;
                             end
-                            OFLOW <= 0; COUT <= 0; {G,L,E} <= `NONE;
-                        end
+
+
 
                         default: begin
                             RES<=0; COUT<=0; OFLOW<=0; {G,L,E}<=`NONE;
+                            ERR <=1;
                         end
                     endcase
-                end
+                end 
             end
         end
     end
 endmodule
-    
+   
